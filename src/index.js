2ecd025269389825089aae0f383722514e40ee62
IreneTaPa <italavero@hotmail.com>
2023-07-24 11:12:34 +0200

    done endpoints

--- src/index.js
+++ src/index.js
@@ -1,47 +1,33 @@
-// Servidor Express
-
-// Para probar los ficheros estáticos del fronend, entrar en <http://localhost:4500/>
-// Para probar el API, entrar en <http://localhost:4500/api/items>
-
 // Imports
 
-const express = require("express");
-const cors = require("cors");
-const mysql = require("mysql2/promise");
-require('dotenv').config()
-
-
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql2/promise');
+const env = require('dotenv');
+env.config();
 
 // Arracar el servidor
-
 const server = express();
 
 // Configuración del servidor
 
 server.use(cors());
-server.use(express.json({limit: "25mb"}));
-server.set('view engine', 'ejs');
-
-
+server.use(express.json({ limit: '25mb' }));
 
 // Conexion a la base de datos
 
 async function getConnection() {
-  const connection = await mysql.createConnection(
-    {
-      host: process.env.DB_HOST || "localhost",
-      user: process.env.DB_USER || "root",
-      password: process.env.DB_PASS,  // <-- Pon aquí tu contraseña o en el fichero /.env en la carpeta raíz
-      database: process.env.DB_NAME || "Clase",
-    }
-  );
+  const connection = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+  });
 
   connection.connect();
 
   return connection;
 }
-
-
 
 // Poner a escuchar el servidor
 
@@ -50,37 +36,113 @@ server.listen(port, () => {
   console.log(`Ya se ha arrancado nuestro servidor: http://localhost:${port}/`);
 });
 
-
-
 // Endpoints
 
-// GET /api/items
+// GET
 
-server.get("/api/items", async (req, res) => {
+server.get('/recetas', async (req, res) => {
+  try {
+    const recipes = req.params.recetas;
+    const selectRecipes = 'SELECT * FROM recetas';
+    const connection = await getConnection();
+    const [results] = await connection.query(selectRecipes, recipes);
+    console.log(results);
+    connection.end();
 
-  const selectProducts = "SELECT * FROM products";
-
-  const conn = await getConnection();
-
-  const [results] = await conn.query(selectProducts);
-
-  console.log(results);
-
-  conn.end();
-
-  res.json(results);
+    res.json({
+      info: {
+        count: results.lenght,
+      },
+      results: results,
+    });
+  } catch (error) {
+    res.json({
+      success: false,
+      message: error,
+    });
+  }
 });
 
-
-
-// GET /details
-
-server.get("/details", async (req, res) => {
-
-  res.render('details', {})
+server.get('/recetas/:id', async (req, res) => {
+  const recipe = req.params.id;
+  const select = 'SELECT * FROM recetas WHERE id = ?';
+  const connection = await getConnection();
+  const [result] = await connection.query(select, recipe);
+  console.log(result);
+  connection.end();
+  res.json({
+    success: true,
+    results: result,
+  });
 });
 
+//
 
-// Serv estáticos
+server.post('/recetas', async (req, res) => {
+  const newRecipe = req.body;
+  try {
+    const insert =
+      'INSERT INTO recetas (nombre, ingredientes, instrucciones) VALUES (?, ?, ?)';
+    const connection = await getConnection();
+    const [result] = await connection.query(insert, [
+      newRecipe.nombre,
+      newRecipe.ingredientes,
+      newRecipe.instrucciones,
+    ]);
+    connection.end();
+    console.log(result);
+    res.json({
+      success: true,
+      id: result.insertId,
+    });
+  } catch (error) {
+    res.json({
+      success: false,
+      message: 'Te faltan campos por rellenar',
+    });
+  }
+});
 
-server.use(express.static("./src/public_html"));
+//
+server.put('/recetas/:id', async (req, res) => {
+  const recipeId = req.params.id;
+  try {
+    const { nameFront, ingFront, instrFront } = req.body;
+    const update =
+      'UPDATE recetas SET nombre = ?, ingredientes = ?, instrucciones = ? WHERE id = ?';
+    const connection = await getConnection();
+    const [result] = await connection.query(update, [
+      nameFront,
+      ingFront,
+      instrFront,
+      recipeId,
+    ]);
+    connection.end();
+    res.json({
+      success: true,
+    });
+  } catch (error) {
+    res.json({
+      success: false,
+    });
+  }
+});
+
+//
+server.delete('/recetas/:id', async (req, res) => {
+  const deleteRecipe = req.params.id;
+  try {
+    const deleteSql = 'DELETE FROM recetas WHERE id = ?';
+    const connection = await getConnection();
+    const [result] = await connection.query(deleteSql, deleteRecipe);
+    connection.end();
+    res.json({
+      success: true,
+    });
+  } catch (error) {
+    res.json({
+      success: false,
+      message: error,
+    });
+  }
+});
